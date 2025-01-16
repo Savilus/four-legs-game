1882ebd8bb90fad2b0bcb7558bf1ee95a7c21171
Savilus <jakublanoszka@gmail.com>
2025-01-16 09:50:33 +0100

    Game init

---
+++ src/main/java/org/example/GamePanel.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import java.awt.*;
+
+import javax.swing.*;
+// Game screen
+public class GamePanel extends JPanel implements Runnable{
+
+  // SCREEN SETTINGS
+  final int originalTitleSize = 16; // 16x16 tile
+  final int scale = 3;
+
+  final int tileSize = originalTitleSize * scale;
+  final int maxScreenColumn = 16;
+  final int maxScreenRow = 12;
+  final int screenWidth = tileSize * maxScreenColumn;
+  final int screenHeight = tileSize * maxScreenRow;
+
+  Thread gameThread;
+
+  public GamePanel() {
+    this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+    this.setBackground(Color.BLACK);
+    this.setDoubleBuffered(true);
+  }
+
+  public void startGameThread() {
+    gameThread = new Thread(this);
+    gameThread.start();
+  }
+
+  @Override
+  public void run() {
+
+  }
+}
