0383a6006fabb5dc39afc9a0250b75d2b0878ae2
Jakub Łanoszka <jakublanoszka@gmail.com>
2025-01-26 22:38:54 +0100

    Objects

---
+++ src/main/java/org/example/object/ChestObject.java
@@ -0,0 +1,17 @@
+package org.example.object;
+
+import javax.imageio.ImageIO;
+import java.io.IOException;
+import java.util.Objects;
+
+public class ChestObject extends SuperObject {
+
+    public ChestObject() {
+        name = "Chest";
+        try {
+            image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/objects/chest.png")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/org/example/object/KeyObject.java
@@ -0,0 +1,18 @@
+package org.example.object;
+
+import java.io.IOException;
+import java.util.Objects;
+
+import javax.imageio.ImageIO;
+
+public class KeyObject extends SuperObject {
+
+    public KeyObject() {
+        name = "Key";
+        try {
+            image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/objects/key.png")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
