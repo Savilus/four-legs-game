806bb302e75cd2a5b0f02a17626aae0dbdee3f05
Savilus <jakublanoszka@gmail.com>
2025-01-27 13:27:26 +0100

    Object interaction

--- src/main/java/org/example/entity/Player.java
+++ src/main/java/org/example/entity/Player.java
@@ -12,128 +12,156 @@ import org.example.KeyHandler;
 
 public class Player extends Entity {
 
-    GamePanel gamePanel;
-    KeyHandler keyHandler;
+  GamePanel gamePanel;
+  KeyHandler keyHandler;
 
-    public final int screenX;
-    public final int screenY;
+  public final int screenX;
+  public final int screenY;
+  int playerKeys = 0;
 
-    public Player(GamePanel gamePanel, KeyHandler keyHandler) {
-        this.gamePanel = gamePanel;
-        this.keyHandler = keyHandler;
+  public Player(GamePanel gamePanel, KeyHandler keyHandler) {
+    this.gamePanel = gamePanel;
+    this.keyHandler = keyHandler;
 
-        screenX = gamePanel.screenWidth / 2 - (gamePanel.tileSize / 2);
-        screenY = gamePanel.screenHeight / 2 - (gamePanel.tileSize / 2);
+    screenX = gamePanel.screenWidth / 2 - (gamePanel.tileSize / 2);
+    screenY = gamePanel.screenHeight / 2 - (gamePanel.tileSize / 2);
 
-        solidArea = new Rectangle();
-        solidArea.x = 8;
-        solidArea.y = 16;
-        solidArea.width = 32;
-        solidArea.height = 32;
+    solidArea = new Rectangle();
+    solidArea.x = 8;
+    solidArea.y = 16;
+    solidAreaDefaultX = solidArea.x;
+    solidAreaDefaultY = solidArea.y;
+    solidArea.width = 32;
+    solidArea.height = 32;
 
-        setDefaultValues();
-        getPlayerImage();
-    }
+    setDefaultValues();
+    getPlayerImage();
+  }
 
-    public void update() {
+  public void update() {
 
-        if (keyHandler.upPressed || keyHandler.downPressed || keyHandler.leftPressed || keyHandler.rightPressed) {
-            if (keyHandler.upPressed) {
-                direction = "up";
-            } else if (keyHandler.downPressed) {
-                direction = "down";
-            } else if (keyHandler.leftPressed) {
-                direction = "left";
-            } else if (keyHandler.rightPressed) {
-                direction = "right";
-            }
-
-            // CHECK TILE COLLISION
-            collisionOn = false;
-            gamePanel.collisionDetector.checkTile(this);
-
-            // IF COLLISION IS FALSE, PLAYER CAN MOVE
-            if (!collisionOn) {
-                switch (direction) {
-                    case "up":
-                        worldY -= speed;
-                        break;
-                    case "down":
-                        worldY += speed;
-                        break;
-                    case "left":
-                        worldX -= speed;
-                        break;
-                    case "right":
-                        worldX += speed;
-                        break;
-                }
-            }
-
-            spriteCounter++;
-            if (spriteCounter > 12) {
-                spriteNum = spriteNum == 1 ? 2 : 1;
-                spriteCounter = 0;
-            }
-        }
-
-    }
-
-    public void getPlayerImage() {
-        try {
-            up1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_up_1.png")));
-            up2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_up_2.png")));
-            down1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_down_1.png")));
-            down2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_down_2.png")));
-            left1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_left_1.png")));
-            left2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_left_2.png")));
-            right1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_right_1.png")));
-            right2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_right_2.png")));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void draw(Graphics2D graphic2d) {
-        BufferedImage image = switch (direction) {
-            case "up" -> {
-                if (spriteNum == 1)
-                    yield up1;
-                else if (spriteNum == 2)
-                    yield up2;
-                yield null;
-            }
-            case "down" -> {
-                if (spriteNum == 1)
-                    yield down1;
-                else if (spriteNum == 2)
-                    yield down2;
-                yield null;
-            }
-            case "left" -> {
-                if (spriteNum == 1)
-                    yield left1;
-                else if (spriteNum == 2)
-                    yield left2;
-                yield null;
-            }
-            case "right" -> {
-                if (spriteNum == 1)
-                    yield right1;
-                if (spriteNum == 2)
-                    yield right2;
-                yield null;
-            }
-            default -> throw new IllegalStateException("Unexpected direction: " + direction);
-        };
-
-        graphic2d.drawImage(image, screenX, screenY, gamePanel.tileSize, gamePanel.tileSize, null);
-    }
-
-    private void setDefaultValues() {
-        worldX = gamePanel.tileSize * 23;
-        worldY = gamePanel.tileSize * 21;
-        speed = 4;
+    if (keyHandler.upPressed || keyHandler.downPressed || keyHandler.leftPressed || keyHandler.rightPressed) {
+      if (keyHandler.upPressed) {
+        direction = "up";
+      } else if (keyHandler.downPressed) {
         direction = "down";
+      } else if (keyHandler.leftPressed) {
+        direction = "left";
+      } else if (keyHandler.rightPressed) {
+        direction = "right";
+      }
+
+      // CHECK TILE COLLISION
+      collisionOn = false;
+      gamePanel.collisionDetector.checkTile(this);
+
+      // CHECK OBJECT COLLISION
+      int objectIndex = gamePanel.collisionDetector.checkObject(this, true);
+      pickUpObject(objectIndex);
+
+      // IF COLLISION IS FALSE, PLAYER CAN MOVE
+      if (!collisionOn) {
+        switch (direction) {
+          case "up":
+            worldY -= speed;
+            break;
+          case "down":
+            worldY += speed;
+            break;
+          case "left":
+            worldX -= speed;
+            break;
+          case "right":
+            worldX += speed;
+            break;
+        }
+      }
+
+      spriteCounter++;
+      if (spriteCounter > 12) {
+        spriteNum = spriteNum == 1 ? 2 : 1;
+        spriteCounter = 0;
+      }
     }
+
+  }
+
+  public void getPlayerImage() {
+    try {
+      up1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_up_1.png")));
+      up2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_up_2.png")));
+      down1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_down_1.png")));
+      down2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_down_2.png")));
+      left1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_left_1.png")));
+      left2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_left_2.png")));
+      right1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_right_1.png")));
+      right2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_right_2.png")));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void draw(Graphics2D graphic2d) {
+    BufferedImage image = switch (direction) {
+      case "up" -> {
+        if (spriteNum == 1)
+          yield up1;
+        else if (spriteNum == 2)
+          yield up2;
+        yield null;
+      }
+      case "down" -> {
+        if (spriteNum == 1)
+          yield down1;
+        else if (spriteNum == 2)
+          yield down2;
+        yield null;
+      }
+      case "left" -> {
+        if (spriteNum == 1)
+          yield left1;
+        else if (spriteNum == 2)
+          yield left2;
+        yield null;
+      }
+      case "right" -> {
+        if (spriteNum == 1)
+          yield right1;
+        if (spriteNum == 2)
+          yield right2;
+        yield null;
+      }
+      default -> throw new IllegalStateException("Unexpected direction: " + direction);
+    };
+
+    graphic2d.drawImage(image, screenX, screenY, gamePanel.tileSize, gamePanel.tileSize, null);
+  }
+
+  public void pickUpObject(int objectIndex) {
+    if (objectIndex != 999) {
+      String objectName = gamePanel.obj[objectIndex].name;
+
+      switch (objectName) {
+        case "Key":
+          playerKeys++;
+          gamePanel.obj[objectIndex] = null;
+          System.out.println("KEY:" + playerKeys);
+          break;
+        case "Door":
+          if (playerKeys > 0) {
+            gamePanel.obj[objectIndex] = null;
+            System.out.println("KEY:" + playerKeys);
+            playerKeys--;
+          }
+      }
+
+    }
+  }
+
+  private void setDefaultValues() {
+    worldX = gamePanel.tileSize * 23;
+    worldY = gamePanel.tileSize * 21;
+    speed = 4;
+    direction = "down";
+  }
 }
